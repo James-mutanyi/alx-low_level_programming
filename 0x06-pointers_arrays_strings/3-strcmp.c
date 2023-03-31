6e630ad01a670499ab0d783c6c0fa76d2da7e3a2
alegiance <alegiance>
2023-03-31 15:33:46 -0700

    java

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code for Holberton School students.
+ * _strcmp - string to compare
+ * @s1: first string
+ * @s2: second string
  *
- * Return: Always 0.
+ * Return: value of string
  */
-int main(void)
+int _strcmp(char *s1, char *s2)
 {
-	char s1[] = "Hello";
-	char s2[] = "World!";
+	int i = 0;
 
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
 }
