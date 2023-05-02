2ba943612567aa85c5047cae79ca26870714225c
alegiance <alegiance>
2023-05-02 14:38:16 -0700

    light

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,20 +1,22 @@
 #include "lists.h"
+
 /**
- * free_listint2 - frees a list but head is null
- * @head: linked list and head
- *
+ * free_listint2 - frees a linked list
+ * @head: pointer to the listint_t list to be freed
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
 	if (head == NULL)
 		return;
-	while (*head != NULL)
+
+	while (*head)
 	{
-		tmp = *head;
+		temp = (*head)->next;
 		free(*head);
-		*head = tmp->next;
+		*head = temp;
 	}
+
 	*head = NULL;
 }
