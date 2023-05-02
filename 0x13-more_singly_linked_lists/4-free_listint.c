71bd45c608a3b8c35eac7cda00e5e26f5929cddd
alegiance <alegiance>
2023-05-02 14:34:08 -0700

    added

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,19 +1,17 @@
 #include "lists.h"
+
 /**
- * free_listint - frees the list
- * @head: head of struct
- *
+ * free_listint - frees a linked list
+ * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
+	listint_t *temp;
 
-
-	while (head != NULL)
+	while (head)
 	{
-		tmp = head;
+		temp = head->next;
 		free(head);
-		head = tmp->next;
+		head = temp;
 	}
-
 }
