d91b58e2bd4552664e68e07494bdb45ab4c3c805
alegiance <alegiance>
2023-05-04 14:04:44 -0700

    get

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H_
-#define HOLBERTON_H_
+ifndef MAIN_H_
+#define MAIN_H_
 #include <stdio.h>
 #include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
@@ -9,4 +9,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
